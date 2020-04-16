010dc26bf1f8e28ee1d7dae62b89d37575edea9f
Иван Варварук <i.varvaruk@pg.local>
2020-04-16 11:23:24 +0300

    First commit

---
+++ src/ievarvaruk/java/proj/JavaHW1/Task4.java
@@ -0,0 +1,8 @@
+package ievarvaruk.java.proj.JavaHW1;
+
+public class Task4 {
+    public static void main (String[] args) {
+        System.out.println("Welcome, Master " + args[0]);
+
+    }
+}
---
+++ src/ievarvaruk/java/proj/Main.java
@@ -0,0 +1,8 @@
+package ievarvaruk.java.proj;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
