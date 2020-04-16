a2757b08b619afb089bea4021990d528ba1989a7
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-16 21:12:45 +0300

    Update Task4.java

--- src/ievarvaruk/java/proj/JavaHW1/Task_4/Task4.java
+++ src/ievarvaruk/java/proj/JavaHW1/Task_4/Task4.java
@@ -1,5 +1,7 @@
 package ievarvaruk.java.proj.JavaHW1;
 
+//  имена пакетов с маленькой буквы
+
 public class Task4 {
     public static void main (String[] args) {
         System.out.println("Welcome, Master " + args[0]);
