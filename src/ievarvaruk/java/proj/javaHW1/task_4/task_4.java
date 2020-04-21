db1a3fd8e9fc5fc615596994b59f3aa4114005c7
WeAreUrgot <varvarukie@gmail.com>
2020-04-21 23:18:12 +0300

    HEEEEEEEEEEY MAKARENAAAAA!!!

---
+++ src/ievarvaruk/java/proj/javaHW1/task_4/task_4.java
@@ -0,0 +1,10 @@
+package ievarvaruk.java.proj.javaHW1.task_4;
+
+//  имена пакетов с маленькой буквы
+
+public class task_4 {
+    public static void main (String[] args) {
+        System.out.println("Welcome, Master " + args[0]);
+
+    }
+}
