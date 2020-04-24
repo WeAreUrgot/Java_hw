1e80689a59b098dc1001856450a220a5f00765f2
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-24 10:11:13 +0300

    Update HW3_Task_2.java

--- src/ievarvaruk/java/proj/javaHW3/task_2/HW3_Task_2.java
+++ src/ievarvaruk/java/proj/javaHW3/task_2/HW3_Task_2.java
@@ -1,20 +1,23 @@
-package ievarvaruk.java.proj.javaHW3.task_2;
+package ievarvaruk.java.proj.javaHW3.task_2; // имена пакетов с маленькой буквы
 
 //Есть массив чисел. Найти среднее арифметическое число элементов массива
 public class HW3_Task_2 {
     public static void main(String[] args) {
         int[] ar = new int[10];
+        
+        // не принципиально, но думаю цикл for здесь бы лучше подошел
         int i = 0;
         while (i < ar.length) {
             ar[i] = (int) (Math.random() * 100); //"Рандомайзер" значений
             System.out.println((i + 1) + "-е значение: " + ar[i]);
             i++;
         }
+        // Sum -> sum - имена переменных с маленькой буквы
         int Sum = 0; //Вычисление ср. арифметического
         for (int val : ar) {
             Sum += val;
         }
-        Sum = Sum / ar.length;
+        Sum = Sum / ar.length; // если получится дробное число, то ответ будет некорректным
         System.out.println("Среднее арифметическое: " + Sum);
     }
 }
