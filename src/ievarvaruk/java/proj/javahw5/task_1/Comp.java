44fb410a39b1fe6778320b5d6a7ccf41d2e4209f
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-05-05 20:01:17 +0300

    Update Comp.java

--- src/ievarvaruk/java/proj/javahw5/task_1/Comp.java
+++ src/ievarvaruk/java/proj/javahw5/task_1/Comp.java
@@ -1,18 +1,19 @@
 package ievarvaruk.java.proj.javahw5.task_1;
 
+// Comp -> ComputerImpl (impl = implementation)
 public class Comp implements CompInt {
 
-//
+// - это зачем?
     @Override
     public void on() {
         System.out.println("Компьютер включен ");
     }
-//
+// - это зачем?
     @Override
     public void off() {
         System.out.println("Компьютер выключен ");
     }
-//
+// - это зачем?
     @Override
     public void reset() {
         System.out.println("Перезагрузка... ");
