6a87dc8cd967ba9c9f5b0eeb2f919ec54372d432
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-05-05 20:07:35 +0300

    Update Warehouse.java

--- src/ievarvaruk/java/proj/javahw5/task_2/Warehouse.java
+++ src/ievarvaruk/java/proj/javahw5/task_2/Warehouse.java
@@ -4,15 +4,18 @@ import java.util.Arrays;
 
 public class Warehouse {
     private Integer[] array;
-//
+//  - это зачем?
     public Warehouse(Integer[] array) {
         this.array = Arrays.copyOf(array, array.length);
     }
-//
+// - это зачем?
     public String toString() {
         return "First Array : " + Arrays.toString(array);
     }
-//
+// - это зачем?
+    /* метод next() по условию должен вернуть 1 элемент массива, поэтому сигнатура должна быть
+    public int next() {}
+    */
     public void next() {
         for (int i = 0; i <= array.length; i++) {
             if (i == array.length) {
@@ -22,7 +25,8 @@ public class Warehouse {
             }
         }
     }
-//
+// - это зачем?
+    // по условию массив нельзя изменять, а ты при помощи геттера предоставил к нему доступ, теперь его можно изменить
     public Integer[] getArray() {
         return array;
     }
